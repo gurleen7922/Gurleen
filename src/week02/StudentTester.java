4083e706f74953246ff372f17383aae07ddc13cd
gurleen7922 <khaigurl@sheridancollege.ca>
2019-05-15 15:35:13 -0400

    Student demo

--- src/week02/StudentTester.java
+++ src/week02/StudentTester.java
@@ -4,19 +4,21 @@
  * and open the template in the editor.
  */
 package week02;
-
+//view class
 public class StudentTester {
     public static void main(String[] args){
-        Student s1 = new Student("yoo");
-        Student s2 = new Student("loo");
-        Student s3 = new Student("too");
+        Student s1 = new Student("yoo",215);
+        Student s2 = new Student("loo", 541);
+        Student s3 = new Student();
+        s3.setName("gaa");
+        s3.setId(842);
         
         Student[] list = new Student[3];
         list[0] = s1;
         list[1] = s2;
         list[2] = s3;
         for(int dex=0; dex<list.length; dex++){
-            System.out.println(list[dex].getName());
+            System.out.println(" " + list[dex].getName() + " " + list[dex].getId());
         }
                 }
 }
